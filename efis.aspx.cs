2750c22282bd31191dcd61e52219aaa222080445
Gem Immanuel <25984676+gemgit7@users.noreply.github.com>
2020-05-09 18:52:56 -0400

    Add files via upload

---
+++ efis.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace avSim
+{
+    public partial class EFISAdapter : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // NOP
+        }
+	}
+}
